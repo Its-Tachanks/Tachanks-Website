0a8080b2c68cb99e9519f10e4710c44825de10ef
Its-Tachanks <joshuaew06@gmail.com>
2022-07-09 21:08:57 -0400

    More Stuff

--- src/Components/counters.jsx
+++ src/Components/counters.jsx
@@ -5,9 +5,9 @@ class Counters extends Component {
   state = {
     counters: [
       { id: 1, value: 4 },
-      { id: 2, value: 0 },
+      /*{ id: 2, value: 0 },
       { id: 3, value: 0 },
-      { id: 4, value: 0 },
+      { id: 4, value: 0 },*/
     ],
   };
   render() {
