55679bbf88ecb2b29d386dcbc13b4d099aad4144
Its-Tachanks <joshuaew06@gmail.com>
2022-07-10 19:26:48 -0400

    Video better?

--- src/index.js
+++ src/index.js
@@ -5,19 +5,17 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap/dist/css/bootstrap.css'
 
 import Counters from './Components/counters'
 import Counter from './Components/counter'
 import Game from './Components/game'
-import VidBg from './Components/VidBg'
-import Home from "./Components/home";
 
 const root = createRoot(document.getElementById('root'))
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<VidBg />} />
+      <Route path="/" element={<Counters />} />
       <Route path="games" element={<Game />} />
     </Routes>
   </BrowserRouter>
