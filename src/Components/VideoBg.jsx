7133b83806b5c7433fa2e136b35f35a766a98472
Its-Tachanks <joshuaew06@gmail.com>
2022-07-12 14:44:51 -0400

    Webm conversion for firefox

--- src/Components/VideoBg.jsx
+++ src/Components/VideoBg.jsx
@@ -6,7 +6,7 @@ class VideoBg extends Component {
     return (
       <div>
         <video autoPlay loop id="VideoBg">
-          <source src={this.getDay()} type="video/mp4" />
+          <source src={this.getDay()} type="video/webm" />
           Video dont work for you
         </video>
       </div>
@@ -16,35 +16,35 @@ class VideoBg extends Component {
   getDay() {
     switch (new Date().getDay()) {
       case 0:
-        return process.env.PUBLIC_URL + "/WeekVideos/sunday.mp4";
+        return process.env.PUBLIC_URL + "/WeekVideos/sunday.webm";
         // eslint-disable-next-line
         break;
       case 1:
-        return process.env.PUBLIC_URL + "/WeekVideos/monday.mp4";
+        return process.env.PUBLIC_URL + "/WeekVideos/monday.webm";
         // eslint-disable-next-line
         break;
       case 2:
-        return process.env.PUBLIC_URL + "/WeekVideos/tuesday.mp4";
+        return process.env.PUBLIC_URL + "/WeekVideos/tuesday.webm";
         // eslint-disable-next-line
         break;
       case 3:
-        return process.env.PUBLIC_URL + "/WeekVideos/wednesday.mp4";
+        return process.env.PUBLIC_URL + "/WeekVideos/wednesday.webm";
         // eslint-disable-next-line
         break;
       case 4:
-        return process.env.PUBLIC_URL + "/WeekVideos/thursday.mp4";
+        return process.env.PUBLIC_URL + "/WeekVideos/thursday.webm";
         // eslint-disable-next-line
         break;
       case 5:
-        return process.env.PUBLIC_URL + "/WeekVideos/friday.mp4";
+        return process.env.PUBLIC_URL + "/WeekVideos/friday.webm";
         // eslint-disable-next-line
         break;
       case 6:
-        return process.env.PUBLIC_URL + "/WeekVideos/saturday.mp4";
+        return process.env.PUBLIC_URL + "/WeekVideos/saturday.webm";
         // eslint-disable-next-line
         break;
       default:
-        return process.env.PUBLIC_URL + "/WeekVideos/sunday.mp4";
+        return process.env.PUBLIC_URL + "/WeekVideos/sunday.webm";
         // eslint-disable-next-line
         break;
     }
