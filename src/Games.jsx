221fea874e58af09d6d5492766b8b30456fdccca
Its-Tachanks <joshuaew06@gmail.com>
2022-07-24 19:30:44 -0400

    games

--- src/Games.jsx
+++ src/Games.jsx
@@ -2,11 +2,18 @@ import React, { Component } from "react";
 
 import Navbar from "./Components/Navbar/Navbar";
 
+import "./Games.css"
+
 class Games extends Component {
   render() {
     return (
       <React.Fragment>
         <Navbar />
+        <iframe
+          id="slope"
+          src="./slope"
+          frameborder="0"
+        ></iframe>
       </React.Fragment>
     );
   }
