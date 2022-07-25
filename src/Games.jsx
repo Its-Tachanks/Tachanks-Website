1b8edbbd85f945bf09ad58843fec6de75d0f45f9
Its-Tachanks <joshuaew06@gmail.com>
2022-07-25 00:47:54 -0400

    Added some games + idk man way too much time spent on nothing

--- src/Games.jsx
+++ src/Games.jsx
@@ -9,13 +9,26 @@ class Games extends Component {
     return (
       <React.Fragment>
         <Navbar />
-        <iframe
-          id="slope"
-          src="./slope"
-          frameborder="0"
-        ></iframe>
+        <embed id="slope" src="./games/subway" frameBorder="0"></embed>
+        <div className="thing">
+          <button onClick={() => this.goFullScreen()}>Fullscreen</button>
+        </div>
       </React.Fragment>
     );
+  }
+
+  goFullScreen() {
+    var elem = document.getElementById("slope");
+
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+      elem.msRequestFullscreen();
+    }
   }
 }
 
